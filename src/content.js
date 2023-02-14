fc0ce0431bfe0f43c42847b932f484432520a2ea
Ilya Ordin <oxilor@gmail.com>
2023-02-14 18:52:44 +0300

    Delete 2 sections: Explore, More from YouTube

--- src/content.js
+++ src/content.js
@@ -1,28 +1,38 @@
+// Redirect the home page to subscriptions
 if (window.location.pathname === '/') {
   const subscriptionsUrl = `${window.location.protocol}//${window.location.host}/feed/subscriptions`;
   window.location.replace(subscriptionsUrl);
 }
 
-function isMenuItemToRemove(ytdGuideEntryRendererNode) {
-  for (const node of ytdGuideEntryRendererNode.childNodes) {
-    if (node.nodeName === 'A' && node.nodeType === Node.ELEMENT_NODE) {
-      return ['Home', 'Shorts'].includes(node.getAttribute('title'));
-    }
-  }
-  return false;
-}
-
+const sectionTagName = 'YTD-GUIDE-SECTION-RENDERER'
 const menuItemTagNames = [
   'YTD-GUIDE-ENTRY-RENDERER',
   'YTD-MINI-GUIDE-ENTRY-RENDERER',
-];
+]
+
+const elementsToDelete = [
+  // Menu items: Home, Shorts
+  (node) => {
+    if (!menuItemTagNames.includes(node.nodeName)) return false
+    return !!node.querySelector('a[title="Home"], a[title="Shorts"]')
+  },
+  // Sections: Explore, More from YouTube
+  (node) => {
+    if (node.nodeName !== sectionTagName) return false
+    const section = node.querySelector('#guide-section-title')
+    if (section) {
+      return ['Explore', 'More from YouTube'].includes(section.textContent)
+    }
+    return false
+  }
+]
+
 const observer = new MutationObserver((mutationList) => {
   for (const mutation of mutationList) {
     for (const node of mutation.addedNodes) {
       if (
-        menuItemTagNames.includes(node.nodeName) &&
         node.nodeType === Node.ELEMENT_NODE &&
-        isMenuItemToRemove(node)
+        elementsToDelete.some(shouldBeDeleted => shouldBeDeleted(node))
       ) {
         node.parentElement?.removeChild(node);
       }
@@ -34,5 +44,5 @@ const menu = document.querySelector('div[id="guide-inner-content"]');
 const collapsedMenu = document.querySelector('ytd-mini-guide-renderer');
 const config = { childList: true, subtree: true };
 
-if (collapsedMenu) observer.observe(collapsedMenu, config);
 if (menu) observer.observe(menu, config);
+if (collapsedMenu) observer.observe(collapsedMenu, config);
